a935ead0b0d7900aa2d2189bffd43a1b2ea6c7a4
dindean <a123deandean@gmail.com>
2020-10-23 23:21:24 -0400

    clean up package.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import HomePage from './HomePage'
 import Header from './Components/Header.jsx';
 import About from './Components/About'
